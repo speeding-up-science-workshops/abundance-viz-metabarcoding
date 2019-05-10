52c3b96b9466725b50d7eb160a0a48887a9ddeea
G-Brennan <g.l.brennan@bangor.ac.uk>
2019-05-10 21:56:30 +0100

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,4 +1,4 @@
-pkgs = c("reshape2", "plyr", "readr", "ggplot2", "RColorBrewer","knitr")
+pkgs = c("reshape2", "plyr", "readr", "ggplot2", "RColorBrewer","knitr", "tidyr")
 ncores = parallel::detectCores()
 install.packages(pkgs, Ncpus = ncores)
 
